821f1e9ee020130a41f45d79060ca04a28b3c8bb
tttonyalpha <tttonyalpha@gmail.com>
2025-04-09 23:26:34 +0300

    added new exp scripts

--- examples/baselines/sac/experiments/1_exp_SWEEP/exp_TriFingerRotateCubeLevel0.sh
+++ examples/baselines/sac/experiments/1_exp_SWEEP/exp_TriFingerRotateCubeLevel0.sh
@@ -10,10 +10,10 @@ seed=(1 2 3)
 for seed in ${seed[@]}
 do
 
-    python examples/baselines/sac/sac.py --env_id="StackCube-v1" --no-track \
-        --total_timesteps=20_000_000  --num_envs=32 --num_eval_envs=100 --eval_freq=10_000 \
+    python examples/baselines/sac/sac.py --env_id="TriFingerRotateCubeLevel0-v1" --no-track \
+        --total_timesteps=30_000_000  --num_envs=32 --num_eval_envs=100 --eval_freq=10_000 \
         --no-partial_reset --no-eval_partial_reset --control_mode="pd_ee_delta_pos" \
-        --utd=0.5 --buffer_size=1_000_000 \
+        --utd=0.5 --buffer_size=1_000_000 --num-steps=250 --num_eval_steps=250 \
         --seed $seed
 
   done
--- examples/baselines/sac/experiments/1_exp_SWEEP/exp_TwoRobotStackCube.sh
+++ examples/baselines/sac/experiments/1_exp_SWEEP/exp_TwoRobotStackCube.sh
@@ -10,10 +10,10 @@ seed=(1 2 3)
 for seed in ${seed[@]}
 do
 
-    python examples/baselines/sac/sac.py --env_id="StackCube-v1" --no-track \
-        --total_timesteps=20_000_000  --num_envs=32 --num_eval_envs=100 --eval_freq=10_000 \
+    python examples/baselines/sac/sac.py --env_id="TwoRobotStackCube-v1" --no-track \
+        --total_timesteps=40_000_000  --num_envs=32 --num_eval_envs=100 --eval_freq=10_000 \
         --no-partial_reset --no-eval_partial_reset --control_mode="pd_ee_delta_pos" \
-        --utd=0.5 --buffer_size=1_000_000 \
+        --utd=0.5 --buffer_size=1_000_000 --num-steps=100 --num_eval_steps=100 \
         --seed $seed
 
   done
