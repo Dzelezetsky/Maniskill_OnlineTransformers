fbfd6a9f96a251af6dd7421168d5452fb10a1bfb
Daniil <zlc.28.02@gmail.com>
2025-04-30 16:12:29 +0300

    fr

--- examples/baselines/sac_nano_gpt/experiments/batch_formation/zero_pad.sh
+++ examples/baselines/sac_nano_gpt/experiments/batch_formation/zero_pad.sh
@@ -5,7 +5,7 @@ seed=(1 2 3)
 for seed in ${seed[@]}
 do
 
-    python ../../D_ver5_NEW_zero_pad.py--env_id="PickCube-v1" --no-track \
+    python ../../D_ver5_NEW_zero_pad.py --env_id="PickCube-v1" --no-track \
         --total_timesteps=1_000_000  --num_envs=5 --num_eval_envs=100 --eval_freq=10_000 \
         --no-partial_reset --no-eval_partial_reset --control_mode="pd_ee_delta_pos" \
         --utd=0.5 --buffer_size=1_000_000 --num-steps=50 --num_eval_steps=50 \
